ffc0e18ecd02f5a17b5a29de1d9ba9cc234fc7cb
AliEmad2002 <99054912+AliEmad2002@users.noreply.github.com>
2023-02-01 02:10:12 +0200

    Update TFT_program_V2.c

--- COTS/Src/HAL/TFT/V2/TFT_program_V2.c
+++ COTS/Src/HAL/TFT/V2/TFT_program_V2.c
@@ -132,7 +132,7 @@ void TFT2_voidInit(
 	 * transistor.
 	 */
 	TIM_u64InitPWM(
-		bcTimUnitNumber, bcTimChannel, TIM_OutputCompareMode_PWM2, 1000000);
+		bcTimUnitNumber, bcTimChannel, TIM_OutputCompareMode_PWM2, 5e6);
 
 	TIM_voidInitOutputPin(bcTimUnitNumber, bcTimChannel, bcTimAFIOMap);
 
